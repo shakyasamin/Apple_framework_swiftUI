ca66c16d6894e43131ed0997d4e86d201c0e663d
saminshakya <saminshakya27@gmail.com>
2025-06-25 13:50:41 +0545

    project created

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
