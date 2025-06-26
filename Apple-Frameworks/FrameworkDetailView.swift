43afaefe29c4f1bee0d939659992640e413a4f3d
saminshakya <saminshakya27@gmail.com>
2025-06-26 12:19:56 +0545

    added safari view

--- Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -12,6 +12,7 @@ struct FrameworkDetailView: View {
     var framework: Framework
     
     @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
     
     var body: some View {
         VStack{
@@ -34,11 +35,14 @@ struct FrameworkDetailView: View {
                 .font(.body)
             Spacer()
             Button {
-                
+                isShowingSafariView = true
             } label: {
                 AFButton(title: "Learn More")
                 
             }
+        }
+        .sheet(isPresented: $isShowingSafariView) {
+            SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
         }
     }
 }
---
+++ Apple-Frameworks/SafariView.swift
@@ -0,0 +1,22 @@
+//
+//  SafariView.swift
+//  Apple-Frameworks
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 26/06/2025.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+}
